10fe6c967b329990d5cdb0c02e7171bd5a14582c
sustechkl <12111012@mail.sustech.edu.cn>
2022-12-13 19:38:32 +0800

    move codes from cpp to hpp

--- matrix.hpp
+++ matrix.hpp
@@ -2,31 +2,102 @@
 #define __MATRIX__
 
 #include <stddef.h> // size_t
+#include <exception> // exception
+#include <iostream> // cerr
 
 template<typename _Tp>
 class __matrix_data {
     private:
+        /**
+         * @brief The number of references to the data.
+        */
         size_t ref;
+        /**
+         * @brief The length of the data.
+        */
         size_t length;
+        /**
+         * @brief The pointer to the head of data.
+        */
         _Tp* data;
     
     public:
-        __matrix_data();
-        __matrix_data(size_t len);
-        ~__matrix_data();
+        __matrix_data(): data(NULL), ref(0), length(0) {}
+        /**
+         * @brief Create a __matrix_data of length len.
+        */
+        __matrix_data(size_t len): __matrix_data(), length(len) {
+            try {
+                data = new _Tp[length];
+            } catch(std::exception &e) {
+                std::cerr << e.what() << std::endl;
+                data = NULL;
+            }
+        }
+        ~__matrix_data() {
+            try {
+                if(data != NULL) delete[] data;
+            } catch(std::exception &e) {
+                std::cerr << e.what() << std::endl;
+            }
+        }
         /**
          * @brief Add a reference to the data.
         */
-        inline void add();
+        inline void add() { ++ref; }
         /**
          * @brief Release a reference to the data. If ref == 0 after this operation, the memory will be released.
         */
-        inline void release();
+        inline void release() {
+            if(ref) {
+                --ref;
+                if(ref == 0 && data != NULL) {
+                    try {
+                        delete[] data;
+                    } catch(std::exception &e) {
+                        std::cerr << e.what() << std::endl;
+                    }
+                }
+            }
+        }
         /**
          * @brief Get the pointer to the data.
          * @return The pointer to the data.
         */
-        inline _Tp* get() const;
+        inline _Tp* get() const { return data; }
+};
+
+/**
+ * @brief Define a pixel in the matrix with more channels. _Tp here SHOULD be a basic data type.
+*/
+template<typename _Tp, size_t _channels>
+class matrix_pixel {
+    public:
+        static const size_t channels = _channels;
+        _Tp arr[_channels];
+        _Tp& operator[] (const size_t id) {
+            return id < channels ? arr[id] : arr[channels - 1];
+        }
+        const _Tp& operator[] (const size_t id) const {
+            return id < channels ? arr[id] : arr[channels - 1];
+        }
+        matrix_pixel<_Tp, _channels> operator+ (const matrix_pixel<_Tp, _channels> &t) const {
+            matrix_pixel<_Tp, _channels> c = *this;
+            for(size_t i = 0; i < channels; ++i)
+                c[i] += t[i];
+            return c;
+        }
+        matrix_pixel<_Tp, _channels> operator- (const matrix_pixel<_Tp, _channels> &t) const {
+            matrix_pixel<_Tp, _channels> c = *this;
+            for(size_t i = 0; i < channels; ++i)
+                c[i] -= t[i];
+            return c;
+        }
+        bool operator== (const matrix_pixel<_Tp, _channels> &t) const {
+            for(size_t i = 0; i < channels; ++i)
+                if((*this)[i] != t[i]) return false;
+            return true;
+        }
 };
 
 template<typename _Tp>
@@ -66,7 +137,26 @@ class matrix {
     /**
      * @brief Release current data.
     */
-    inline void release();
+    inline void release() {
+        if(__data != NULL) {
+            __data->release();
+            __data = NULL;
+            __begin = NULL;
+        }
+    }
+
+    /**
+     * @brief Do a soft copy.
+    */
+    inline void copy(const matrix<_Tp> &t) {
+        release();
+        this->rows = t.rows;
+        this->columns = t.columns;
+        this->gap = t.gap;
+        this->__data = t.__data;
+        this->__begin = t.__begin;
+        this->__data->add();
+    }
 
     public:
 
@@ -75,21 +165,27 @@ class matrix {
     /**
      * @brief Default constructor. It will set all pointers to NULL to avoid errors.
     */
-    matrix();
+    matrix(): __data(NULL), __begin(NULL), rows(0), columns(0), gap(0) {}
 
     /**
      * @brief Create a new matrix with rows = row and columns = column.
     */
-    matrix(size_t row, size_t column);
+    matrix(size_t row, size_t column): matrix(), rows(row), columns(column), gap(column) {
+        __data = __matrix_data<_Tp>(row * column);
+        __data->add();
+        __begin = __data->get();
+    }
 
     /**
      * @brief (Soft) copy constructor.
     */
-    matrix(const matrix<_Tp> &t);
+    matrix(const matrix<_Tp> &t): matrix() {
+        this->copy(t);
+    }
 
     /* Destructor */
     
-    ~matrix();
+    ~matrix() { release(); }
     
     /* Functions */
 
@@ -97,25 +193,43 @@ class matrix {
      * @brief Get the number of rows.
      * @return The number of rows.
     */
-    inline size_t getRows() const;
+    inline size_t getRows() const { return this->rows; }
 
     /**
      * @brief Get the number of columns.
      * @return The number of columns.
     */
-    inline size_t getColumns() const;
+    inline size_t getColumns() const { return this->columns; }
 
     /**
      * @brief Do a hard copy. Use operator= instead to do a soft copy.
     */
-    matrix<_Tp>& clone() const;
+    matrix<_Tp>& clone() const {
+        matrix t = new matrix();
+        t.rows = rows;
+        t.gap = t.columns = columns;
+        t.__data = new __matrix_data<_Tp>(rows * columns);
+        _Tp *ptr = t.__begin = t.__data->get(), *sptr = __begin;
+        /**
+         * @note You cannot make a deep copy of __matrix_data here, because
+         * this matrix can be just a ROI of some larger matrix.
+        */
+        for(int i = 0; i < t.rows; ++i) {
+            for(int j = 0; j < t.columns; ++j) *(ptr + j) = *(sptr + j);
+            ptr += t.columns;
+            sptr += gap;
+        }
+        return t;
+    }
 
     /* Operators */
 
     /**
      * @brief Do a soft copy. Use clone() instead to do a hard copy.
     */
-    void operator= (const matrix<_Tp> &t);
+    void operator= (const matrix<_Tp> &t) {
+        this->copy(t);
+    }
 
 };
 
---
+++ report.md
@@ -0,0 +1,28 @@
+# Project 5 -  A Class for Matrices
+
+**SID:** 12111012 **Name:** 匡亮(KuangLiang)
+
+----
+
+#### Part One - Introduction and Highlights
+
+这个 project 中，我实现了 ```matrix``` 类。它可以存储一个二维的矩阵，支持多种数据类型（包括自定义的多通道像素类 ```matrix_pixel``` ），支持常见的几种运算符，支持软拷贝和硬拷贝。
+
+矩阵的具体内容并不存储在矩阵类中，而是存储在另一个类 ```__matrix_data``` 中。这个类会记录自己被多少个矩阵访问，当访问数降到 $0$ 的时候就释放记录内容占用的内存。因此 ```matrix``` 的软拷贝和设置 ROI 都是 O(1) 时间复杂度的。
+
+如果采用了 C++11 或更高的标准，则可以使用一些适合新版本的功能，如 ```forEach``` 等。如果不采用，同样可以通过编译。
+
+#### Part Two - Classes and Interfaces
+
+1. ```class __matrix_data```
+   
+用户不应该用这个类，因此不提供文字说明（但代码中有完整的成员变量解释和接口说明）。
+
+2. ```template<typename _Tp, size_t _channels> class matrix_pixel```
+
+一个元素类型为 _Tp ，通道数为 _channels 的像素点，_Tp 应为基本数据类型之一，否则它需要重载过 += 、-= 、== 这三个运算符。
+
+3. ```template<typename _Tp> class matrix```
+
+一个元素类型为 _Tp 的矩阵，建议 _Tp 为基本数据类型之一或 matrix_pixel 。
+
