c008327d3d474f34453aefe104b086dd190f1fef
sustechkl <12111012@mail.sustech.edu.cn>
2022-12-14 15:34:51 +0800

    finished most of functions

--- matrix.hpp
+++ matrix.hpp
@@ -85,6 +85,11 @@ class matrix_pixel {
         const _Tp& operator[] (const size_t id) const {
             return id < channels ? arr[id] : arr[channels - 1];
         }
+        const _Tp& operator= (const _Tp t) {
+            for(size_t i = 0; i < channels; ++i)
+                (*this)[i] = t;
+            return (*this);
+        }
         matrix_pixel<_Tp, _channels> operator+ (const matrix_pixel<_Tp, _channels> &t) const {
             matrix_pixel<_Tp, _channels> c = *this;
             for(size_t i = 0; i < channels; ++i)
@@ -132,7 +137,7 @@ class matrix {
         _Tp* __begin;
 
         /**
-         * @brief The ROI of this matrix. 0 <= ROI_row_begin < ROI_row_end <= getRows(), same for column.
+         * @brief The ROI of this matrix. 0 <= ROI_row_begin < ROI_row_end <= getOriginalRows(), same for column.
         */
         size_t ROI_row_begin, ROI_row_end, ROI_column_begin, ROI_column_end;
 
@@ -175,16 +180,7 @@ class matrix {
         /**
          * @brief Create a new matrix with rows = row and columns = column.
         */
-        matrix(size_t row, size_t column): matrix() {
-            __data = __matrix_data<_Tp>(row, column);
-            if(__data != NULL) {
-                ROI_row_begin = ROI_column_begin = 0;
-                ROI_row_end = row - 1;
-                ROI_column_end = column - 1;
-                __data->add();
-                __begin = __data->get();
-            }
-        }
+        matrix(size_t row, size_t column): matrix() { create(row, column); }
 
         /**
          * @brief (Soft) copy constructor.
@@ -203,46 +199,72 @@ class matrix {
          * @brief Get the number of rows of the original matrix.
          * @return The number of rows.
         */
-        inline size_t getRows() const { return __data == NULL ? 0 : __data->rows; }
+        inline size_t getOriginalRows() const { return __data == NULL ? 0 : __data->rows; }
 
         /**
          * @brief Get the number of columns of the original matrix.
          * @return The number of columns.
         */
-        inline size_t getColumns() const { return __data == NULL ? 0 : __data->columns; }
+        inline size_t getOriginalColumns() const { return __data == NULL ? 0 : __data->columns; }
 
         /**
          * @brief Get the number of rows of the ROI, or, the matrix now.
          * @return The number of rows.
         */
-        inline size_t getROIrows() const { return ROI_row_end - ROI_row_begin; }
+        inline size_t getRows() const { return ROI_row_end - ROI_row_begin; }
 
         /**
          * @brief Get the number of columns of the ROI, or, the matrix now.
          * @return The number of columns.
         */
-        inline size_t getROIcolumns() const { return ROI_column_end - ROI_column_begin; }
+        inline size_t getColumns() const { return ROI_column_end - ROI_column_begin; }
+        
+        /**
+         * @brief Create a new matrix with rows = row and columns = column.
+        */
+        void create(size_t row, size_t column) {
+            release();
+            __data = __matrix_data<_Tp>(row, column);
+            if(__data != NULL) {
+                ROI_row_begin = ROI_column_begin = 0;
+                ROI_row_end = row - 1;
+                ROI_column_end = column - 1;
+                __data->add();
+                __begin = __data->get();
+            }
+        }
 
         /**
          * @brief Do a hard copy. Use operator= instead to do a soft copy.
         */
         matrix<_Tp> clone() const {
             matrix t = new matrix();
-            t.__data = new __matrix_data<_Tp>(getRows(), getColumns());
+            t.__data = new __matrix_data<_Tp>(getOriginalRows(), getOriginalColumns());
             t.ROI_row_begin = t.ROI_column_begin = 0;
-            t.ROI_row_end = getRows() - 1;
-            t.ROI_column_end = getColumns() - 1;
+            t.ROI_row_end = getOriginalRows() - 1;
+            t.ROI_column_end = getOriginalColumns() - 1;
             _Tp *ptr = t.__begin = t.__data->get(), *source_ptr = __begin;
             /**
              * @note You cannot make a deep copy of __matrix_data here, because
              * this matrix can be just a ROI of some larger matrix.
             */
-            for(int i = 0; i < t.rows; ++i) {
-                for(int j = 0; j < t.columns; ++j) *(ptr + j) = *(source_ptr + j);
+            for(size_t i = 0; i < t.rows; ++i) {
+                for(size_t j = 0; j < t.columns; ++j) *(ptr + j) = *(source_ptr + j);
                 ptr += t.columns;
-                source_ptr += getColumns();
+                source_ptr += getOriginalColumns();
             }
             return t;
+        }
+
+        /**
+         * @brief Set all elements to zero.
+        */
+        void setZero() {
+            for(size_t i = 0; i < getRows(); ++i) {
+                register _Tp *ptr_a = (*this)[i];
+                for(size_t j = 0; j < getColumns(); ++j)
+                    ptr_a[j] = 0;
+            }
         }
 
         /**
@@ -252,10 +274,12 @@ class matrix {
             if(__data == NULL || t.__data == NULL) return false;
             if(__data == t.__data && ROI_column_begin == t.ROI_column_begin && ROI_column_end == t.ROI_column_end
                 && ROI_row_begin == t.ROI_row_begin && ROI_row_end == t.ROI_row_end) return true;
-            for(int i = 0; i < getROIrows(); ++i)
-                for(int j = 0; j < getROIcolumns(); ++j)
-                    if((*this)[i][j] != t[i][j])
+            for(size_t i = 0; i < getRows(); ++i) {
+                register _Tp *ptr_a = (*this)[i], *ptr_b = t[i];
+                for(size_t j = 0; j < getColumns(); ++j)
+                    if(*ptr_a[j] != *ptr_b[j])
                         return false;
+            }
             return true;
         }
         
@@ -266,10 +290,12 @@ class matrix {
             if(__data == NULL || t.__data == NULL) return false;
             if(__data == t.__data && ROI_column_begin == t.ROI_column_begin && ROI_column_end == t.ROI_column_end
                 && ROI_row_begin == t.ROI_row_begin && ROI_row_end == t.ROI_row_end) return true;
-            for(int i = 0; i < getROIrows(); ++i)
-                for(int j = 0; j < getROIcolumns(); ++j)
+            for(size_t i = 0; i < getRows(); ++i) {
+                register _Tp *ptr_a = (*this)[i], *ptr_b = t[i];
+                for(size_t j = 0; j < getColumns(); ++j)
                     if(!equ((*this)[i][j], t[i][j]))
                         return false;
+            }
             return true;
         }
 
@@ -278,12 +304,17 @@ class matrix {
          * The up and left borders will be move first, and then the down and right borders cannot be less than them.
         */
         void adjust_ROI(int move_up, int move_down, int move_left, int move_right) {
-            ROI_row_begin = max(0, min(getRows() - 1, ROI_row_begin + move_up));
-            ROI_column_begin = max(0, min(getColumns() - 1, ROI_column_begin + move_left));
-            ROI_row_end = max(ROI_row_begin, min(getRows() - 1, ROI_row_end + move_up));
-            ROI_column_end = max(ROI_column_begin, min(getColumns() - 1, ROI_column_end + move_left));
-            if(__data != NULL) __begin = __data->get() + ROI_row_begin * getColumns() + ROI_column_begin;
+            ROI_row_begin = max(0, min(getOriginalRows() - 1, ROI_row_begin + move_up));
+            ROI_column_begin = max(0, min(getOriginalColumns() - 1, ROI_column_begin + move_left));
+            ROI_row_end = max(ROI_row_begin, min(getOriginalRows() - 1, ROI_row_end + move_up));
+            ROI_column_end = max(ROI_column_begin, min(getOriginalColumns() - 1, ROI_column_end + move_left));
+            if(__data != NULL) __begin = __data->get() + ROI_row_begin * getOriginalColumns() + ROI_column_begin;
         }
+
+        /**
+         * @brief Get the element at row = r, column = c.
+        */
+        _Tp at(size_t r, size_t c) { return (*this)[r][c]; }
 
         /* Operators */
 
@@ -308,17 +339,17 @@ class matrix {
         /**
          * @brief Get the pointer of the row_th row of the matrix.
         */
-        _Tp* operator[] (size_t row) const { return __begin + min(row, getROIrows() - 1) * getColumns(); }
+        _Tp* operator[] (size_t row) const { return __begin + min(row, max(getRows(), 1) - 1) * getOriginalColumns(); }
 
         /**
          * @brief Do this = this + t if this and t have the same size.
         */
         matrix<_Tp>& operator+= (const matrix<_Tp> &t) {
             if(this->__data != NULL && t.__data != NULL 
-                && this->getROIrows() == t.getROIrows() && this->getROIcolumns() == t.getROIcolumns()) {
-                    for(int i = 0; i < getROIrows(); ++i) {
+                && this->getRows() == t.getRows() && this->getColumns() == t.getColumns()) {
+                    for(size_t i = 0; i < getRows(); ++i) {
                         _Tp *ptr = t[i], *source_ptr = (*this)[i];
-                        for(int j = 0; j < getROIcolumns(); ++j)
+                        for(size_t j = 0; j < getColumns(); ++j)
                             *(ptr + j) += *(source_ptr + j);
                     }
             }
@@ -338,10 +369,10 @@ class matrix {
         */
         matrix<_Tp>& operator-= (const matrix<_Tp> &t) {
             if(this->__data != NULL && t.__data != NULL 
-                && this->getROIrows() == t.getROIrows() && this->getROIcolumns() == t.getROIcolumns()) {
-                    for(int i = 0; i < getROIrows(); ++i) {
+                && this->getRows() == t.getRows() && this->getColumns() == t.getColumns()) {
+                    for(size_t i = 0; i < getRows(); ++i) {
                         _Tp *ptr = t[i], *source_ptr = (*this)[i];
-                        for(int j = 0; j < getROIcolumns(); ++j)
+                        for(size_t j = 0; j < getColumns(); ++j)
                             *(ptr + j) -= *(source_ptr + j);
                     }
             }
@@ -356,6 +387,22 @@ class matrix {
             return s -= t;
         }
 
+        /**
+         * @brief Do a matrix multiplication.
+        */
+        matrix<_Tp> operator* (const matrix<_Tp> &t) const {
+            size_t M = getRows(), K = getColumns(), N = t.getColumns(); // (M x K) * (K x N)
+            matrix<_Tp> s = new matrix(M, N);
+            s.setZero();
+            if(t.getRows() == K) {
+                for(size_t i = 0; i < M; ++i)
+                    for(size_t k = 0; k < K; ++k) {
+                        register _Tp element_a = (*this)[i][k], *ptr_s = s[i], *ptr_b = t[k];
+                        for(size_t j = 0; j < N; ++j) ptr_s[j] += element_a * ptr_b[j];
+                    }
+            }
+            return s;
+        }
 };
 
 #endif /*__MATRIX__*/
